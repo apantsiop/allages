65a880fc10c9e681e1c08b6597804b4dde7accfa
Apostolos Pantsiopoulos <apantsiop@yahoo.gr>
2019-08-23 15:29:15 +0300

    Added yaml generator

---
+++ lib/generators/yaml_generator.rb
@@ -0,0 +1,28 @@
+require 'rails/generators/base'
+
+module Allages
+  class YamlGenerator < Rails::Generators::Base
+    desc "This generator initializes the allages gem with this directory"
+
+    # def create_initializer_file
+    #   create_file "config/initializers/.allages.yml", <<~CONFIG
+    #   # input_dir: 'changelogs'
+    #   # output_file: 'stdout'
+    #   # include_unreleased: false
+    #   # header: "<<~END
+    #   # # Changelog
+    #   # All notable changes to this project will be documented in this file.
+
+    #   # The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+    #   # and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+    #   # END"
+    #   CONFIG
+    # end
+
+    def create_directories_structure
+      empty_directory Allages.config.input_dir
+      empty_directory "#{Allages.config.input_dir}/Unreleased"
+    end
+  end
+end
